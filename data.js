5fe0397444265e00d92e3de0af9a1f4859f24afd
aradhya <aradhyapatro14@gmail.com>
2021-10-29 19:42:17 +0530

    bare complete

--- data.js
+++ data.js
@@ -25,6 +25,12 @@ const contact={
     "description":"dolores ducimus nobis adipisci non!Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste voluptatibus architecto corrupti! Inventore quam veritatis eos, mollitia nesciunt eveniet id excepturi quas, assumenda quia, ab dignissimos quod temporibus ipsa labore dolore sit esse tempora. Maiores quas cumque deserunt dolore optio dolores ducimus nobis adipisci non!"
 };
 
+const home={
+    "Title": "Home",
+    "description": "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur tenetur possimus aut numquam placeat, ad dolor quia ipsa, nostrum excepturi ex accusantium nulla illum iure obcaecati labore tempora, sed in soluta? Debitis consequuntur laborum laboriosam! Dignissimos eum atque unde aspernatur iure fugit repellat voluptatibus quisquam libero. Asperiores id odit eaque hic placeat, sint quod nam tenetur quo amet. Placeat dolorem temporibus sit fugiat voluptatem omnis soluta praesentium ducimus, nostrum adipisci ea nam veniam repellendus quidem porro doloremque facilis corrupti alias optio nisi. Explicabo quaerat odio mollitia eveniet incidunt dolore dolor, maiores commodi? Delectus, at! Dolore necessitatibus minima est qui voluptatum."
+}
+
 module.exports.about=about;
 module.exports.contact=contact;
+module.exports.home=home;
 module.exports.data = data;
